abffbb2624c0121603c7ab53f97f6ab7b6c571dd
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-09-28 10:07:03 +0530

    Update tree file: Add find method & Update JSDoc Comments

--- tree.js
+++ tree.js
@@ -2,6 +2,11 @@ import { node } from "./node.js";
 import { merge } from "../project-recursion/merge-sort.js";
 
 const tree = function (array) {
+  /** Create a tree structure using the given array
+   * @array {Array} - Sorted array
+   * @start {Number} - Starting Index (normally first index)
+   * @end {Number} - Last Index (normally last index)
+   */
   const buildTree = function (array, start, end) {
     if (start > end) return null;
     const midIndex = Math.floor((start + end) / 2);
@@ -12,6 +17,26 @@ const tree = function (array) {
     currentRoot.right = buildTree(array, midIndex + 1, end);
 
     return currentRoot;
+  };
+
+  /** This function will find the given value inside the tree, 
+   *  else will throw an error 
+   *  @rootNode {node}
+   *  @value {Number}
+   * */
+  const find = function (rootNode, value) {
+    let curr = rootNode;
+    if (curr.data === value) return curr;
+    while(curr != null) {
+        if (curr.data > value) {
+            curr = curr.left;
+        } else if (curr.data < value) {
+            curr = curr.right;
+        } else {
+            return curr;
+        }
+    }
+    throw Error("Data not found");
   };
 
   // Helper Functions
@@ -30,9 +55,9 @@ const tree = function (array) {
     }
   };
 
-  /** Sort given array based on the situation  
+  /** Sort given array based on the situation
    * @clean {true} if providing initial array has no duplicates.
-   * @insert {true} if a new node is inserted into the tree 
+   * @insert {true} if a new node is inserted into the tree
    */
   const sortArray = function (newArray, clean = false, insert = false) {
     if (!clean) {
@@ -47,9 +72,10 @@ const tree = function (array) {
   let sortedArray = sortArray(array);
   const root = buildTree(sortedArray, 0, sortedArray.length - 1);
 
-  return { root, prettyPrint };
+  return { root, find, prettyPrint };
 };
 
 const list = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const bst = tree(list);
 bst.prettyPrint(bst.root);
+console.log(bst.find(bst.root, 4));
